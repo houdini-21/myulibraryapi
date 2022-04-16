1d6a6dd4e329579e45833eba5279b5bd0f77fa01
houdini-21 <marinerocruzfer@gmail.com>
2022-04-16 05:25:13 -0600

    make it better

--- api/index.js
+++ api/index.js
@@ -10,6 +10,11 @@ const PORT = 3001;
 var cors = require("cors");
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(expressSanitizer());
 require("./libs/database/database");
 
