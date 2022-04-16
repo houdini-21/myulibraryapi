9b562cebe30e57e8e1489f15967bc3d950bf3bde
houdini-21 <marinerocruzfer@gmail.com>
2022-04-16 05:28:18 -0600

    make it better

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ var cors = require("cors");
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use(expressSanitizer());
